30cda9e43f3271dceb5b218755beb0fd631f1d42
Darren Dahl & Mathilde Mouw <pair+darrendahl.mathildemouw@devbootcamp.com>
2014-03-20 16:58:01 -0700

    MVP! search finds some nearby restaurant...
      * Connects to Google maps API and google Places API to find user
      geolocation and restaurants

--- app/assets/javascripts/restaurants.js
+++ app/assets/javascripts/restaurants.js
@@ -1,7 +1,12 @@
 $(document).ready(function(){
-  var myuser = new User();
-  myuser.getPos();
-  console.log(myuser.position)
+  var myUser = new User();
+  myUser.getPos();
+
+  $('input').on('click', function(e){
+    e.preventDefault()
+    HelpMe.showNearbyRestaurants(myUser.position);
+  })
+
 });
 
 
@@ -17,6 +22,26 @@ User.prototype = {
       user.setPos(position)});
   },
   setPos: function(geolocation){
-    this.position.coords = geolocation.coords
+    this.position = new google.maps.LatLng( geolocation.coords.latitude, geolocation.coords.longitude);
   }
+}
+
+
+
+var HelpMe = {}
+
+HelpMe.showNearbyRestaurants = function(position){
+  var service = new google.maps.places.PlacesService(document.getElementById('result'))
+  // var nearbyArea = new google.maps.places.PlacesService.nearbySearch(position)
+  // console.log(nearbyArea)
+  var opts = {
+    location: position,
+    // radius: 3000,
+    types: ['restaurant', 'food', 'cafe'],
+    rankBy: google.maps.places.RankBy.DISTANCE
+  }
+
+  service.nearbySearch(opts, function(response){
+    $('#result').append(response[0].name)
+  });
 }
