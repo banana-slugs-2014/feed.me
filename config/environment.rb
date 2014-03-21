34eed04ec3a7556b544526a4931c5b9df15f1906
Darren Dahl & Mathilde Mouw <pair+darrendahl.mathildemouw@devbootcamp.com>
2014-03-20 18:06:24 -0700

    fixes environment.rb file

--- config/environment.rb
+++ config/environment.rb
@@ -8,11 +8,17 @@ HelpMe::Application.initialize!
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
 APP_NAME = APP_ROOT.basename.to_s
 
+
+
+
+
 unless Rails.env.test?
   env_config = YAML.load_file(APP_ROOT.join('config', 'facebook.yml'))
   env_config.each do |key,value|
     ENV[key] = value
   end
+
+
   config_google = YAML.load_file(APP_ROOT.join('config', 'google_keys.yml'))
 
   config_google.each do |key,value|
