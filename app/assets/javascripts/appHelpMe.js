4a93f0795e51e99db5b27cbda0f5778a91d1b19b
Darren Dahl & Mathilde Mouw <pair+darrendahl.mathildemouw@devbootcamp.com>
2014-03-20 19:51:58 -0700

    Implements MVC for js find restauraunt functionality

---
+++ app/assets/javascripts/appHelpMe.js
@@ -0,0 +1,19 @@
+var HelpMe = {}
+
+
+$(document).ready(function(){
+
+  var myUser = new User();
+
+  var Selectors = {
+    resultContainerSelector: '#result',
+    helpMeButtonSelector: '.helpMeButton'
+  }
+
+  HelpMe.view = new HelpMe.View(Selectors);
+  HelpMe.controller = new HelpMe.Controller(HelpMe.view, myUser);
+  new HelpMe.Binder(Selectors, HelpMe.controller).bind();
+
+});
+
+
