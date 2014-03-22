05558d48896757a76ff5544c5b0329905fa28367
Phil Vargas <null>
2014-03-22 15:55:34 -0700

    in userauth.js   -add the javascript view to the controller input   -change the body.html functionality to the view and call the view function in the controller   -add facebook scraping function to the controller   -add user route for update   -refactor routes for places

--- app/assets/javascripts/useroauth.js
+++ app/assets/javascripts/useroauth.js
@@ -1,7 +1,8 @@
 var App = {}
 
-App.Controller = function(facebook){
+App.Controller = function(facebook, view){
   this.facebook = facebook
+  this.view = view
 };
 
 App.Controller.prototype = {
@@ -11,30 +12,49 @@ App.Controller.prototype = {
         type: "DELETE",
         url: '/logout',
       }).done(function(response){
-      console.log('logging out')
-      //this.view.doSomething
+        console.log('logging out')
       })
 
     });
   },
 
   logIn: function(){
+    var self = this
     this.facebook.login(function(response){
       $.ajax({
         type: "POST",
         url: '/login',
         data: response
       }).done(function(views){
-        $('body').html(views)
+        console.log('calling facebook info')
+        self.getFacebookInfo()
+        self.view.renderUpdate(views)
       });
     },{scope: 'user_likes,user_checkins'});
   },
+
+  getFacebookInfo: function(){
+    this.facebook.api('/me?fields=id,name,checkins,age_range,gender,location,currency,interests,likes',
+      function(response){
+        if (response && !response.error){
+          $.ajax({
+            type: "PUT",
+            url: '/users/'+response.id,
+            data: response
+          }).done(function(data){
+            console.log('All your base are belong to us!')
+          })
+        }
+      })
+  }
 }
 
 App.View = function(){}
 
 App.View.prototype = {
-
+  renderUpdate: function(data){
+    $('body').html(data)
+  }
 }
 
 App.Binder = function(controller){
--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,15 @@
 HelpMe::Application.routes.draw do
 
 
-  root :to => 'users#index'
-  post '/places' => 'places#create'
-  get  '/places' => 'places#index', as: :places
-  get  '/places/:id' => 'places#show', as: :places_path
+  root to: "users#index"
+  # post '/places' => 'places#create'
+  # get  '/places' => 'places#index', as: :places
+  # get  '/places/:id' => 'places#show', as: :places_path
+  resources :places, only: [:index, :create, :show]
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
-  root to: "users#index"
   post '/login', to: 'sessions#create', as: :login
   delete '/logout', to: 'sessions#destroy', as: :logout
-  resources :users
-
+  resources :users, only: [:update]
 end
