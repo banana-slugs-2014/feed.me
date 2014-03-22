3bfd2ffeb4a8383c680010f30908fdcc2d015775
Phil Vargas <null>
2014-03-21 20:56:32 -0700

    add allow mass assign unit tests for place add serialize unit tests for place

--- spec/models/places_spec.rb
+++ spec/models/places_spec.rb
@@ -6,4 +6,16 @@ describe Place do
     it {should have_many :user_places}
     it {should have_many(:users).through :user_places}
   end
-end
+
+  context "serialize" do
+    it {should serialize :types}
+  end
+
+  context "mass assignment (attr_accessible)" do
+    it {should allow_mass_assignment_of :name}
+    it {should allow_mass_assignment_of :address}
+    it {should allow_mass_assignment_of :types}
+    it {should allow_mass_assignment_of :latitude}
+    it {should allow_mass_assignment_of :longitude}
+  end
+ end
