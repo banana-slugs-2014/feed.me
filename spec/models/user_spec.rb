4677185f7551c9248c11d649a1d785a4dcfdd8a9
Phil Vargas <null>
2014-03-21 19:40:13 -0700

    create user for unit testing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 
 describe User do
- it {should validate_presence_of :uid}
- it {should validate_uniqueness_of :uid}
- it {should validate_presence_of :oauth_token}
+  let!(:myuser){FactoryGirl.create :user}
+  it {should validate_presence_of :uid}
+  it {should validate_uniqueness_of :uid}
+  it {should validate_presence_of :oauth_token}
 end
