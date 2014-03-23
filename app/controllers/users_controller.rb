0e90679b176248a19aed0e92909e167c4c0a6e17
Phil Vargas <null>
2014-03-22 18:15:21 -0700

    move create route from session to user add route for create user as /login add specs to cover change

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
+  def create
+    user = User.find_by_uid(params[:authResponse][:userID])
+    if user.blank?
+      user = User.new
+      user.uid = params[:authResponse][:userID]
+      user.oauth_token = params[:authResponse][:accessToken]
+      #consider removing if else protection on production
+      if user.save
+        session[:user_id] = user.uid
+        render json: {id: user.id}
+      else
+        redirect_to root_path
+      end
+    else
+      session[:user_id] = user.uid
+      render partial: "places/index", locals: { user_id: session[:user_id]}
+    end
+  end
+
   def update
     #pending update user
     user = User.find(params[:id])
