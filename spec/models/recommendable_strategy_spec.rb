e10c6972d828ffd266e54bd5dc56d49e77154db7
Harry Whelchel <harrywhelchel@gmail.com>
2014-03-24 09:54:27 -0700

    Update recommendable strategy test. Stub user#recommended_places method. Test pass

--- spec/models/recommendable_strategy_spec.rb
+++ spec/models/recommendable_strategy_spec.rb
@@ -1,15 +1,15 @@
-# require 'spec_helper'
+require 'spec_helper'
 
-# describe RecommendableStrategy do
+describe RecommendableStrategy do
 
-#   let(:recommendable) { create(:recommendable_strategy) }
-#   let(:my_recommender) { build(:recommender) }
-#   let(:my_place) { create(:place) }
+  let(:recommendable) { create(:recommendable_strategy) }
+  let(:my_recommender) { build(:recommender) }
+  let(:my_place) { create(:place) }
 
-#   context '#recommend' do
-#     it 'returns one place' do
-#       my_recommender.user.like my_place
-#       expect(recommendable.recommend(my_recommender)).to be_a_kind_of Place
-#     end
-#   end
-# end
+  context '#recommend' do
+    it 'returns one place' do
+      my_recommender.user.stub(:recommended_places) { [my_place] }
+      expect(recommendable.recommend(my_recommender)).to be_a_kind_of Place
+    end
+  end
+end
