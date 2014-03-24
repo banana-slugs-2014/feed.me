86857bbd4e465a673056bd29a24a92aadd22faf0
Steven G. Harms <steven@devbootcamp.com>
2014-03-24 11:08:02 -0700

    strategy tips

--- app/models/simple_google_places.rb
+++ app/models/simple_google_places.rb
@@ -1,4 +1,8 @@
 class SimpleGooglePlaces < RecommendationStrategy
+  # I like that you're going here, but you might want to consider using subdirs
+  # to lay this out.  Put recommendation_strategy.rb at the top level but then
+  # have recommendation_strategy/{google.rb, facebook.rb} etc.
+  #
 
   def recommend(data)
     a_close_restaurant
