2fe95844399aec8190b73380e7f9f909c42b2707
Harry Whelchel <apprentice@dbc33.local>
2014-03-23 22:02:05 -0700

    Add after_filter to update recommendable redis table

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -1,11 +1,18 @@
 class RecommendationsController < ApplicationController
+  after_filter :update_recommendable_redis, only: :update
 
   # I think we should use new, create, and edit actions here
   # for the core user experience instead of places actions.
 
   def update
-    RecommendableWorker.perform_async(current_user, Place.find(params[:id]))
+    recommendation = Recommendation.find(params[:id])
+    recommendation.like_recommendation
   end
 
+  private
+
+  def update_recommendable_redis
+    RecommendableWorker.perform_async(current_user, recommendation.place)
+  end
 
 end
