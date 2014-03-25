1a8ad64bb8dbffcc99823cfdb735cb86ec955858
Johnny Wu <jwu@Johnnys-MacBook-Pro.local>
2014-03-25 09:50:03 -0700

    testing for heroku push

--- config/environment.rb
+++ config/environment.rb
@@ -11,27 +11,27 @@ APP_NAME = APP_ROOT.basename.to_s
 
 
 
-unless Rails.env.test?
-  env_config = YAML.load_file(APP_ROOT.join('config', 'facebook.yml'))
-  env_config.each do |key,value|
-    ENV[key] = value
-  end
+# unless Rails.env.test?
+#   env_config = YAML.load_file(APP_ROOT.join('config', 'facebook.yml'))
+#   env_config.each do |key,value|
+#     ENV[key] = value
+#   end
 
-  config_yelp = YAML.load_file(APP_ROOT.join('config', 'yelp.yml'))
-  config_yelp.each do |key,value|
-    ENV[key] = value
-  end
+#   config_yelp = YAML.load_file(APP_ROOT.join('config', 'yelp.yml'))
+#   config_yelp.each do |key,value|
+#     ENV[key] = value
+#   end
 
-  config_google = YAML.load_file(APP_ROOT.join('config', 'google_keys.yml'))
+#   config_google = YAML.load_file(APP_ROOT.join('config', 'google_keys.yml'))
 
-  config_google.each do |key,value|
-    ENV[key] = value
-  end
+#   config_google.each do |key,value|
+#     ENV[key] = value
+#   end
 
-  config_foursquare = YAML.load_file(APP_ROOT.join('config', 'foursquare.yml'))
+#   config_foursquare = YAML.load_file(APP_ROOT.join('config', 'foursquare.yml'))
 
-  config_foursquare.each do |key,value|
-    ENV[key] = value
-  end
+#   config_foursquare.each do |key,value|
+#     ENV[key] = value
+#   end
 
-end
+# end
