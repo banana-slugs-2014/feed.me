15edaa2cae9ca343c1f1eac9117b6e35660135f4
Harry Whelchel <apprentice@dbc21.local>
2014-03-24 18:18:49 -0700

    Add simple foursquare strategy and update tests.   * Take foursquare data from api call and save to database if   they are not new.   * Update tests to reflect Recommender and recommendation addition to   in Places create action.

--- spec/models/recommendable_strategy_spec.rb
+++ spec/models/recommendable_strategy_spec.rb
@@ -3,7 +3,8 @@ require 'spec_helper'
 describe RecommendableStrategy do
 
   let(:recommendable) { create(:recommendable_strategy) }
-  let(:my_recommender) { build(:recommender) }
+  let(:my_user){ create(:user) }
+  let(:my_recommender) { Recommender.new(my_user,[my_place]) }
   let(:my_place) { create(:place) }
   let(:my_other_place){ create :place }
   let(:my_places){ [my_place, my_other_place] }
@@ -12,7 +13,7 @@ describe RecommendableStrategy do
     context 'with a recommended place' do
       it 'returns one place' do
         my_recommender.user.stub(:recommended_places) { [my_place] }
-        expect(recommendable.recommend(my_recommender)).to be_a_kind_of Place
+        expect(recommendable.recommend(my_recommender)).to be_an_instance_of Place
       end
     end
 
