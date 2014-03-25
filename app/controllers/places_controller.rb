68ae014f8370db676705ce886df175b653687b65
Phil V <philipavargas@gmail.com>
2014-03-25 15:59:49 -0700

    fix typo in places controller trials ==> trial

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -34,7 +34,7 @@ class PlacesController < ApplicationController
     end
 
     trial = ab_test('Recommendation Strategy', *StrategyTester.strategies).constantize
-    recommendation = Recommender.new(current_user, places, strategy: trials).recommend
+    recommendation = Recommender.new(current_user, places, strategy: trial).recommend
     render partial: 'show', locals: { recommendation: recommendation }
   end
 
