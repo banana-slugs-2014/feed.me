5540e20c3f10ab96687e28ae2f25751adb9d2801
Phil V <philipavargas@gmail.com>
2014-03-25 13:46:08 -0700

    add an extra test for facebook_weighted_spec when user has an age

--- spec/models/facebook_weighted_spec.rb
+++ spec/models/facebook_weighted_spec.rb
@@ -1,15 +1,24 @@
 require 'spec_helper'
 
 describe FacebookWeighted do
-  let(:my_user){ create(:user) }
+  let(:my_user){ create(:user, age_range: 21) }
   let(:my_place) { create(:place) }
+  let(:beer_place) { create(:place, types: ['Brewery']) }
+  let(:pizza_place) { create(:place, types: ['Pizza Place']) }
+  let(:mexican_place) { create(:place, types: ['Mexican Restaurant']) }
+  let(:grocery_place) { create(:place, types: ['Grocery Store']) }
+  let(:my_places){ [beer_place, pizza_place, mexican_place, grocery_place, my_place] }
   let(:my_recommender) { Recommender.new(my_user,[my_place]) }
+  let(:my_age_recommender) { Recommender.new(my_user,my_places) }
   let(:fb_weighted){ create(:facebook_weighted) }
 
   context '#recommend' do
     it "should return a place" do
-      fb_weighted.stub(:weighted_random){my_place}
       expect(fb_weighted.recommend(my_recommender)).to be_an_instance_of Place
+    end
+
+    it "should return a place the user is 21" do
+      expect(fb_weighted.recommend(my_age_recommender)).to be_an_instance_of Place
     end
   end
 end
