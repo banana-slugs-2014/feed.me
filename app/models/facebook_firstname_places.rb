59bc22e4a0f2c9b1a9c59eb2604fdae05af29f2b
Mathilde Mouw <nihaomouw@mac.com>
2014-03-25 14:57:56 -0700

    Add test for Facebook letter match strategy
      * change FacebookLocationPlaces to
      FacebookFirstnamePlaces
      * matching the first letter of the first name instead of location
      is much more awesome
      * specs pass

---
+++ app/models/facebook_firstname_places.rb
@@ -0,0 +1,21 @@
+class FacebookFirstnamePlaces < RecommendationStrategy
+
+  attr_reader :user, :places
+
+  def recommend(data)
+    @user = data.user
+    @places = data.places
+
+    first_letter_in_common
+  end
+
+  def first_letter_in_common
+    firstname_letter = user.name[0]
+    firstname_places = []
+    places.each do |place|
+      firstname_places << place if (place.name[0] == firstname_letter) || (place.address[0] == firstname_letter)
+    end
+    p firstname_places
+    firstname_places.empty? ? places.sample : firstname_places.sample
+  end
+end
