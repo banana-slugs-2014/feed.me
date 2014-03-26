b16e266155fd9958204489d6f25fbe793303aa79
Phil V <philipavargas@gmail.com>
2014-03-26 13:00:01 -0700

    disolve case statement and replaced with & comparators in facebook weighted

--- app/models/recommendation_strategies/facebook_weighted.rb
+++ app/models/recommendation_strategies/facebook_weighted.rb
@@ -8,7 +8,7 @@ class FacebookWeighted < RecommendationStrategy
 
   def weighted_random(data)
     weighted_initial = Hash[data.places.map{ |place| [place, 3] }]
-    weighted_places = update_weights(data.user, weighted_initial)
+    weighted_places = update_weights!(data.user, weighted_initial)
     select_weighted_place(weighted_places)
   end
 
@@ -23,29 +23,24 @@ class FacebookWeighted < RecommendationStrategy
     selected_place.first
   end
 
-  def update_weights(user, normalized_data)
-    update_weights = normalized_data.inject({}) do |updated, (place, weight)|
-      if user.age_range == 21
-        case place.types.first
-        when 'Brewery' then weight += 2
-        when 'Irish Pub' then weight += 2
-        when 'Beer Garden' then weight += 2
-        when 'Gastropub' then weight += 2
-        when 'Pizza Place' then weight += 1
-        when 'Burger Joint' then weight += 1
-        when 'Taco Place' then weight += 1
-        when 'Mexican Restaurant' then weight += 1
-        when 'Grocery Store' then weight -= 2
-        else weight
-        end
-      end
-    
+  def update_weights!(user, normalized_data)
+    update_weights = normalized_data.inject({}) do |memo, (place, weight)|
+      
+      weight += age_21_weights!(place.types) if user.age_range == 21
       (weight += 3) if user.user_likes.pluck(:name).include?(place.name)
       (weight += 3) if user.checkins.pluck(:name).include?(place.name)
         
-      updated = updated.merge(place => weight)
+      memo = memo.merge(place => weight)
     end
     update_weights
+  end
+
+  def age_21_weights!(types)
+    weight = 0
+    weight += (['Brewery','Irish Pub','Beer Garden','Gastropub'] & types).length * 2    
+    weight += (['Pizza Place','Burger Joint','Taco Place','Mexican Restaurant'] & types).length    
+    weight -= (['Grocery Store'] & types).length
+    weight    
   end
 
 end
