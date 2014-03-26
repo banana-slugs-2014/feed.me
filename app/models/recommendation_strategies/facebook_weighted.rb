d4fe4346489ea458bacfe76b7b16723775f7f2f9
Phil V <philipavargas@gmail.com>
2014-03-26 12:34:06 -0700

    spike for fb weighted

--- app/models/recommendation_strategies/facebook_weighted.rb
+++ app/models/recommendation_strategies/facebook_weighted.rb
@@ -23,22 +23,52 @@ class FacebookWeighted < RecommendationStrategy
     selected_place.first
   end
 
+
+  class FbWeightCalculator
+    BASELINE_WEIGHT = 3
+
+    def initialize(type_bearer, age_bearer)
+      @types = type_bearer.types
+      @age_range = age_bearer.age_range
+      @weight = BASELINE_WEIGHT
+    end
+
+    def weight
+      privilege_fun_places_for_young_people!
+      privilege_likes!
+    end
+
+    private
+
+    def privilege_fun_places_for_young_people!
+      return unless @age_range == 21
+      @weight += ([ 'Brewery','Irish Pub','Beer Garden','Gastropub' ] & @types).length * 2   
+    
+
+#rinse repeat
+        #   when 'Pizza Place','Burger Joint','Taco Place','Mexican Restaurant' then weight += 1
+        #   when 'Grocery Store' then weight -= 2
+        #   else weight
+        # end
+    end
+
+    def privilege_likes!
+    end
+
+  end
+
+  # FbWeightCalculator(place, user).weight # => 3
+
   def update_weights(user, normalized_data)
     update_weights = normalized_data.inject({}) do |updated, (place, weight)|
-      if user.age_range == 21
-        case place.types.first
-        when 'Brewery' then weight += 2
-        when 'Irish Pub' then weight += 2
-        when 'Beer Garden' then weight += 2
-        when 'Gastropub' then weight += 2
-        when 'Pizza Place' then weight += 1
-        when 'Burger Joint' then weight += 1
-        when 'Taco Place' then weight += 1
-        when 'Mexican Restaurant' then weight += 1
-        when 'Grocery Store' then weight -= 2
-        else weight
-        end
-      end
+      #group the categories into similar weights
+      #place.types.any?{|type| categories.include?(type)}
+    
+    # aggregate_weight = normalized_data.reduce(:+) do |place, weight|
+    #   FbWeightCalculator(place, user).weight
+    # end
+
+      weight = FbWeightCalculator(place, user).weight
     
       (weight += 3) if user.user_likes.pluck(:name).include?(place.name)
       (weight += 3) if user.checkins.pluck(:name).include?(place.name)
