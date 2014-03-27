0d5bcea4f94dfce421ddb0da0da569b9488af5cf
Harry Whelchel <harrywhelchel@gmail.com>
2014-03-26 20:12:59 -0700

    Configure travis yaml for travis ci to heroku deploy integration

--- config/initializers/01_redis.rb
+++ config/initializers/01_redis.rb
@@ -1,4 +1,4 @@
-uri = URI.parse(ENV["REDISTOGO_URL"])
 if Rails.env.production?
+  uri = URI.parse(ENV["REDISTOGO_URL"])
   REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 end
