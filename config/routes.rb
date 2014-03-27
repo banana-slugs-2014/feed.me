2719581f09b62413792dec470280224daa686ca4
Phil V <philipavargas@gmail.com>
2014-03-27 11:04:18 -0700

    remove extraneous places routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ HelpMe::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
   root to: "users#index"
-  resources :places, only: [:index, :create, :show]
+  resources :places, only: [:create]
   resources :recommendations, only: [:update]
 
   post '/login', to: 'users#create', as: :login
